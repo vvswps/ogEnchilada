f0f495b7650762df3c2a3361d4fb55aaab7ad37d
vvswps <as904465@gmail.com>
2020-12-12 12:31:33 +0530

    Fixed the question spam still few issues

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # botBABA
+
+## This is a Discord bot
+
+Its supposed to respond on mention of few keywords (does that)
+And it is supposed to handle applications for my COD clan (miserable performance for now)
--- bot.py
+++ bot.py
@@ -2,48 +2,11 @@
 
 import discord
 from discord.ext import commands
+import json
 
-TOKEN = open('token.txt', 'r').readline()
 
 # client = discord.Client()
-client = commands.Bot('.')
-
-
-class application_manager:
-
-    def __init__(self):
-        self.responses = []
-        self.application = {}
-        print(bool(self.application))
-
-    def addResponse(self, message):
-        self.message = message
-        self.responses.append(self.message)
-
-    def compile(self, discord_id):
-        self.application[f'{discord_id}'] = self.responses
-
-    def fetch(self, discord_id):
-        return self.application[f'{discord_id}']
-        print(bool(self.application))
-
-    def fetchAll(self):
-        return self.application
-        print(bool(self.application))
-
-    def checkDict(self):
-        return bool(self.application)
-
-
-# @client.command(name='helpme')
-# async def help(context):
-#     help_embed = discord.Embed(title='Help', description='Command list for the application bot', color=0xff0000) # noqa
-#     help_embed.add_field(name='.apply', value='To begin an application process', inline=True) # noqa
-#     help_embed.add_field(name='.helpme', value='To view this help message', inline=True) # noqa
-#     help_embed.set_author(name='BABAji')
-#     help_embed.set_footer(text='For further queries or Bug Reports DM BABAji')
-
-#     await context.message.channel.send(embed=help_embed)
+client = commands.Bot('.', help_command=None)
 
 
 questions = [
@@ -54,26 +17,80 @@ questions = [
             "5. What are your Current and Personal Best ranks respectively?",
             "6. BR or MP: Which one do you prefer more?",
             "7. Number of matches you play per day (on an average)?",
-            "8. How would you describe yourself as a player (Casual or Competitive)?", # noqa
+            "8. How would you describe yourself as a player (Casual or Competitive)?",
             "9. What is your preferred weapon class?",
-            "10. Would you be willing to use voice communication during ranked matches or Clan in-house scrims?", # noqa
-            "11. In brief, describe why do you wanna join us and what would be willing to offer to the clan?", # noqa
-            "12. Make your in-game profile and battle logs unhidden. It might take 1-2 days for your application to be reviewed by the Recruiters (we promise to do it ASAP). Please type anything to continue." # noqa
+            "10. Would you be willing to use voice communication during ranked matches or Clan in-house scrims?",
+            "11. In brief, describe why do you wanna join us and what would be willing to offer to the clan?",
+            "12. Make your in-game profile and battle logs unhidden. It might take 1-2 days for your application to be reviewed by the Recruiters (we promise to do it ASAP). Please type anything to continue."
             ]
+
+
+class application_manager:
+
+    def __init__(self):
+        self.responses = []
+        d = open('data.json', 'r+')
+        self.application = json.load(d)
+        self.current_app = {}
+        # print(bool(self.application))
+
+    def addResponse(self, message):
+        self.responses.append(message)
+
+    def compile(self, discord_id):
+        e = dict(zip(questions, self.responses))
+        self.current_app[f'{discord_id}'] = e
+
+    def fetch(self, discord_id):
+        return self.application[f'{discord_id}']
+        # print(bool(self.application))
+
+    def fetchAll(self):
+        for appln in self.application:
+            return appln
+        # print(bool(self.application))
+
+    def check(self):
+        return bool(self.application)
+
+
+@client.command(name='help')
+async def help(context):
+    help_embed = discord.Embed(title='Help', description='Command list for the application bot', color=0x101020)
+
+    help_embed.add_field(name='.apply', value='To begin an application process', inline=True)
+
+    help_embed.add_field(name='.helpme', value='To view this help message', inline=True)
+
+    help_embed.set_author(name='BABAji')
+    help_embed.set_footer(text='For further queries or Bug Reports DM BABAji')
+
+    await context.message.channel.send(embed=help_embed)
 
 
 @client.command(name='apply')
 async def apply(context):
 
     await context.send('Fill the application through your DMs')
-    apply_embed = discord.Embed(title='Enchiladas Application', description='So you wanna apply for the clan! Good choice\nAnswer the following questions to finish the application', color=0x00ff00) # noqa
-    await context.message.author.send(embed=apply_embed) # noqa
+    apply_embed = discord.Embed(title='Enchiladas Application', description='So you wanna apply for the clan! Good choice\nAnswer the following questions to finish the application', color=0x00ff00)
+
+    await context.message.author.send(embed=apply_embed)
 
     mngr = application_manager()
     for question in questions:
         await context.message.author.send(question)
-        resp = await client.wait_for('message')
-        mngr.addResponse(resp.content)
+        while 0 < 1:
+            resp = await client.wait_for('message')
+            if resp.author == context.message.author:
+            # if resp.content != question:
+                print(resp.content)
+                mngr.addResponse(resp.content)
+                break
+            else:
+                # await context.message.author.send("Sorry some error occurred")
+                continue
+
+    mngr.compile(context.message.author)
     await context.message.author.send(embed=discord.Embed(description='Finished application', color=0x00ff00))
 
     # await context.message.author.send("So this is your application:\n")
@@ -84,7 +101,7 @@ async def apply(context):
 @client.command(name='review')
 async def review(context):
     mngr = application_manager()
-    if mngr.checkDict is False:
+    if mngr.check is False:
         await context.message.channel.send(embed=discord.Embed(title='No applications to review', color=0x00ff00))
     else:
         await context.message.author.send(embed=discord.Embed(title='Enter the discord id of applicant you wanna review', color=0x00ff00))
@@ -96,7 +113,7 @@ async def review(context):
 @client.command(name='reviewAll')
 async def reviewAll(context):
     mngr = application_manager()
-    if mngr.checkDict is False:
+    if mngr.check is False:
         await context.message.author.send(embed=discord.Embed(title='No applications to review', color=0x00ff00))
     else:
         await context.message.author.send(mngr.fetchAll())
@@ -107,20 +124,22 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name='.help'))
     print(f'{client.user} has connected to Discord!')
 
-    # general_channel = client.get_channel(758167336544239640)
+    # general_channel = client.get_channel(channel_id)
     # await general_channel.send('Baba ki jai ho!')
 
 
 @client.event
 async def on_message(message):
     if 'baba' in message.content:
-        # general_channel = client.get_channel(758167336544239640)
+        # general_channel = client.get_channel(channel_id)
         # await general_channel.send('Baba ki jai ho!')
-        await message.channel.send("Baba ki jai ho!")   # This will work for all channels Unlike the above code that would only work for general channel # noqa
+        await message.channel.send("Baba ki jai ho!")   # This will work for all channels Unlike the above code that would only work for general channel
 
     elif 'babaji' in message.mentions:
         await message.channel.send("Who dares!!!")
 
     await client.process_commands(message)
 
-client.run(TOKEN)
+
+with open('token.txt', 'r') as TOKEN:
+    client.run(TOKEN.readline())
