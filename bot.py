616857fec537a8b7fa9454f233a5fd689071ee2d
vvswps <as904465@gmail.com>
2021-05-16 00:01:38 +0530

    idk whats changed lets see

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@
 import discord
 from discord.ext import commands
 import json
+import pandas as pd
 
 
 # client = discord.Client()
@@ -17,11 +18,11 @@ questions = [
             "5. What are your Current and Personal Best ranks respectively?",
             "6. BR or MP: Which one do you prefer more?",
             "7. Number of matches you play per day (on an average)?",
-            "8. How would you describe yourself as a player (Casual or Competitive)?",
+            "8. How would you describe yourself as a player (Casual or Competitive)?", # noqa
             "9. What is your preferred weapon class?",
-            "10. Would you be willing to use voice communication during ranked matches or Clan in-house scrims?",
-            "11. In brief, describe why do you wanna join us and what would be willing to offer to the clan?",
-            "12. Make your in-game profile and battle logs unhidden. It might take 1-2 days for your application to be reviewed by the Recruiters (we promise to do it ASAP). Please type anything to continue."
+            "10. Would you be willing to use voice communication during ranked matches or Clan in-house scrims?", # noqa
+            "11. In brief, describe why do you wanna join us and what would be willing to offer to the clan?", # noqa
+            "12. Make your in-game profile and battle logs unhidden. It might take 1-2 days for your application to be reviewed by the Recruiters (we promise to do it ASAP). Please type anything to continue." # noqa
             ]
 
 
@@ -30,6 +31,16 @@ class application_manager:
     def __init__(self):
         self.responses = []
         d = open('data.json', 'r+')
+
+        # df = pd.read_csv("C:/Users/as904/Documents/VScode/Discord bot/applications.csv", index_col=0)
+        '''
+        read json
+        append json
+        check if json is empty
+        if yes tell there's nothing to show
+        otherwise show the recent entry first and so on
+        '''
+
         self.application = json.load(d)
         self.current_app = {}
         # print(bool(self.application))
@@ -56,11 +67,11 @@ class application_manager:
 
 @client.command(name='help')
 async def help(context):
-    help_embed = discord.Embed(title='Help', description='Command list for the application bot', color=0x101020)
+    help_embed = discord.Embed(title='Help', description='Command list for the application bot', color=0x101020) # noqa
 
-    help_embed.add_field(name='.apply', value='To begin an application process', inline=True)
+    help_embed.add_field(name='.apply', value='To begin an application process', inline=True) # noqa
 
-    help_embed.add_field(name='.helpme', value='To view this help message', inline=True)
+    help_embed.add_field(name='.help', value='To view this help message', inline=True) # noqa
 
     help_embed.set_author(name='BABAji')
     help_embed.set_footer(text='For further queries or Bug Reports DM BABAji')
@@ -72,7 +83,7 @@ async def help(context):
 async def apply(context):
 
     await context.send('Fill the application through your DMs')
-    apply_embed = discord.Embed(title='Enchiladas Application', description='So you wanna apply for the clan! Good choice\nAnswer the following questions to finish the application', color=0x00ff00)
+    apply_embed = discord.Embed(title='Enchiladas Application', description='So you wanna apply for the clan! Good choice\nAnswer the following questions to finish the application', color=0x00ff00) # noqa
 
     await context.message.author.send(embed=apply_embed)
 
@@ -81,17 +92,18 @@ async def apply(context):
         await context.message.author.send(question)
         while 0 < 1:
             resp = await client.wait_for('message')
-            if resp.author == context.message.author:
-            # if resp.content != question:
+            if resp.guild is None and resp.author == context.message.author:
+                # message.guild will be None if it's not in a guild
+                # if resp.author == context.message.author:
+                # if resp.content != question:
                 print(resp.content)
                 mngr.addResponse(resp.content)
-                break
-            else:
-                # await context.message.author.send("Sorry some error occurred")
-                continue
+            # else:
+            #     # await context.message.author.send("Sorry some error occurred") # noqa
+            #     continue
 
     mngr.compile(context.message.author)
-    await context.message.author.send(embed=discord.Embed(description='Finished application', color=0x00ff00))
+    await context.message.author.send(embed=discord.Embed(description='Finished application', color=0x00ff00)) # noqa
 
     # await context.message.author.send("So this is your application:\n")
     # await context.message.author.send()
@@ -102,9 +114,9 @@ async def apply(context):
 async def review(context):
     mngr = application_manager()
     if mngr.check is False:
-        await context.message.channel.send(embed=discord.Embed(title='No applications to review', color=0x00ff00))
+        await context.message.channel.send(embed=discord.Embed(title='No applications to review', color=0x00ff00)) # noqa
     else:
-        await context.message.author.send(embed=discord.Embed(title='Enter the discord id of applicant you wanna review', color=0x00ff00))
+        await context.message.author.send(embed=discord.Embed(title='Enter the discord id of applicant you wanna review', color=0x00ff00)) # noqa
 
         id = await client.wait_for('message')
         await context.message.author.send(mngr.fetch(id))
@@ -114,9 +126,17 @@ async def review(context):
 async def reviewAll(context):
     mngr = application_manager()
     if mngr.check is False:
-        await context.message.author.send(embed=discord.Embed(title='No applications to review', color=0x00ff00))
+        await context.message.author.send(embed=discord.Embed(title='No applications to review', color=0x00ff00)) # noqa
     else:
         await context.message.author.send(mngr.fetchAll())
+
+
+@client.event
+async def on_command_error(context, exception):
+    if isinstance(exception, commands.CommandNotFound):
+        await context.message.delete()
+        # pass
+        await context.send(embed=discord.Embed(description='No such command exists. Try .help', color=0x0000ff)) # noqa
 
 
 @client.event
@@ -124,19 +144,15 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name='.help'))
     print(f'{client.user} has connected to Discord!')
 
-    # general_channel = client.get_channel(channel_id)
-    # await general_channel.send('Baba ki jai ho!')
-
 
 @client.event
 async def on_message(message):
-    if 'baba' in message.content:
-        # general_channel = client.get_channel(channel_id)
-        # await general_channel.send('Baba ki jai ho!')
-        await message.channel.send("Baba ki jai ho!")   # This will work for all channels Unlike the above code that would only work for general channel
+    if 'baba' in message.content.lower():
+        await message.channel.send("Baba ki jai ho!")
 
-    elif 'babaji' in message.mentions:
-        await message.channel.send("Who dares!!!")
+    # mention = '613451817510109205'
+    if '613451817510109205' in message.content:
+        await message.channel.send("Who Dares!!!")
 
     await client.process_commands(message)
 
---
+++ fixes.md
@@ -0,0 +1,9 @@
+# Changelog?
+
+## 4-1-2021
+
+    Fixed an issue where the application bot would take answers from both DM channel and the server it was called from
+
+## 5-1-2021
+
+    Catches command not found error now
